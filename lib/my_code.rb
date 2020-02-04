858b91486458597a8aff8bae291c8a047100b1c8
Owen Abbott <ocabbott@gmail.com>
2020-02-04 17:18:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -28,7 +28,7 @@ def map_to_square(source_array)
   return new_array
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point=0)
   total = starting_point
   for i in source_array
   total += i 
