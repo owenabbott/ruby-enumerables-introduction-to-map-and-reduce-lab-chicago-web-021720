ae97a2ddae8fac4d6b052c1803bd292d3960cd73
Owen Abbott <ocabbott@gmail.com>
2020-02-04 17:12:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,5 +8,29 @@ return new_array
 end
 
 def map_to_no_change(source_array)
-  return source_array
+  new_array=source_array
+  return new_array
+end
+
+def map_to_double(source_array)
+  new_array=[]
+  for i in source_array
+  new_array.push(source_array*2)
+  end
+end
+
+def map_to_square(source_array)
+  new_array=[]
+  for i in source_array
+  new_array.push(source_array**2)
+  end
+end
+
+def reduce_to_total(source_array, starting_point)
+end
+
+def reduce_to_all_true(source_array)
+end
+
+reduce_to_any_true(source_array)
 end
