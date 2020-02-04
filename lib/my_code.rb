5eef04770a3d4ce7ea3f0129b885de70d424ed9c
Owen Abbott <ocabbott@gmail.com>
2020-02-04 17:28:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,6 +39,12 @@ end
 
 def reduce_to_all_true(source_array)
   pp source_array
+  for i in source_array
+  if i = false 
+    return false
+  else
+    return true
+  end
 end
 
 def reduce_to_any_true(source_array)
